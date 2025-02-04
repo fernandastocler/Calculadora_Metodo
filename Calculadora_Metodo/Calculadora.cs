dc2e77ada1da45a08186dc588ca5890d6f3d1925
fernanda stocler <nandasastocler@gmail.com>
2025-02-04 16:00:36 -0300

    adicionando calculadora

---
+++ Calculadora_Metodo/Calculadora.cs
@@ -0,0 +1,13 @@
+namespace Calculadora
+{
+    class Operacoes
+    {
+        public static void RealizarAdicao(
+            decimal valor1,
+            decimal valor2
+       )
+        {
+                Console.WriteLine ($"A somas dos valores {valor1} e {valor2} é {valor1+valor2}");
+                }
+    }
+}
---
+++ Calculadora_Metodo/Ola.cs
@@ -0,0 +1,10 @@
+namespace Ola
+{
+ class Cumprimento
+  {
+    public static void ImprimirOla()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+  }
+}
